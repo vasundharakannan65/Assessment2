55feafee9ae70966fa933639f2baf01577d17e45
Vasundhara S Kannan <vasundharask@gislen.com>
2021-11-19 18:18:36 +0530

    Validations pending

---
+++ CustomerFeedBackFormMVC/CustomerFeedBackFormMVC/Controllers/FeedbackController.cs
@@ -0,0 +1,46 @@
+﻿using CustomerFeedBackFormMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerFeedBackFormMVC.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private readonly CustomerSatisfactoryFeedBackFormContext _db;
+
+        public FeedbackController(CustomerSatisfactoryFeedBackFormContext db)
+        {
+            this._db = db;
+        }
+        // GET: Feedback/Create
+        public IActionResult Create()
+        {
+            //ViewBag.brands = _assetBL.listOfBrands();
+            //ViewBag.brands = _assetBL.listOfBrands();
+            IEnumerable<SelectListItem> listOfProducts = _db.Products.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+
+            });
+
+            IEnumerable<SelectListItem> listOfCategory = _db.Categories.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+
+            });
+
+
+            ViewBag.products = listOfProducts;
+            ViewBag.category = listOfCategory;
+
+
+            return View();
+        }
+    }
+}
---
+++ CustomerFeedBackFormWebAPI/CustomerFeedBackFormWebAPI/Interfaces/IFeedbackRepository.cs
@@ -0,0 +1,13 @@
+﻿using CustomerFeedBackFormWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerFeedBackFormWebAPI.Interfaces
+{
+    public interface IFeedbackRepository
+    {
+        void Create(CustomerForm custerForm);
+    }
+}
